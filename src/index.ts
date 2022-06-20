5a563fef0a27a7d3e000450dff8146c1b3220b33
alonNftrade <94784334+alonNftrade@users.noreply.github.com>
2022-06-20 15:06:17 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import api from './api';
 import transactions from './transactions';
 import sign from './signature';
 import { setWallet } from './wallet/setWallet';
+import { findChainById } from './utils/chain';
 
 interface Test {
   test: string
@@ -12,14 +13,17 @@ const nftdaoSDK = ({
 }) => {
   let data;
   const _transactions = transactions(marketplaceId);
+
   const setWeb3 = async (provider: any, providerType: 'EVM' | 'immutablex' | 'solana') => {
     await setWallet(provider, providerType);
     await _transactions.setWalletData(data);
   };
+
   return {
     api: api,
     transactions: _transactions,
     sign,
+    findChain:(chain :number) => findChainById(chain),
     test: (payload: Test) => console.log(payload),
     setWeb3,
   }
