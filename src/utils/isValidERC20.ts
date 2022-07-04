dbd4908fd315d5172a399d385984bb4bc6343899
alonNftrade <94784334+alonNftrade@users.noreply.github.com>
2022-07-04 13:23:57 +0300

    add type for isvaliderc20

--- src/utils/isValidERC20.ts
+++ src/utils/isValidERC20.ts
@@ -1,4 +1,4 @@
 import currencies from "./currencies";
 
-export const isValidERC20 = (ERC20Contract, chainId) => (currencies.find(currency => currency.address.toLowerCase() === ERC20Contract.toLowerCase() && currency.chainId === chainId))
+export const isValidERC20 = (ERC20Contract: string, chainId: number | string) => (currencies.find(currency => currency.address.toLowerCase() === ERC20Contract.toLowerCase() && currency.chainId === chainId))
 
