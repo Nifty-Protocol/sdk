0bbdd16f884b010a0668871e23c0447603ac4116
alonNftrade <94784334+alonNftrade@users.noreply.github.com>
2022-07-04 13:06:30 +0300

    fix init wallet bug

--- src/wallet/index.ts
+++ src/wallet/index.ts
@@ -1,5 +1,4 @@
 import { EVM, IMMUTABLEX, SOLANA } from '../utils/chains';
-import { findChainById } from '../utils/chain';
 import { Wallet } from './Wallet';
 import Evm from './walletProvider/evm';
 import ImmutableX from './walletProvider/ImmutableX';
@@ -11,7 +10,7 @@ const web3Libs = {
   // [SOLANA]: Solana,
 };
 
-export default function (provider: any, type: string): Wallet {
+export default function (type: string, provider: any): Wallet {
   const web3Lib = web3Libs[type];
   if (!web3Lib) {
     throw new Error('unknown provider type');
