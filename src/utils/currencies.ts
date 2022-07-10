899cb56e5573eca3a495361bf409f41a255bfd9b
alonNftrade <94784334+alonNftrade@users.noreply.github.com>
2022-07-10 15:25:16 +0300

    add default payment method

--- src/utils/currencies.ts
+++ src/utils/currencies.ts
@@ -4,36 +4,42 @@ export default [
     address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
     key: 'WETH',
     value: 'Wrraped Ether',
+    defaultPaymentMethod:true,
   },
   {
     chainId: 56,
     address: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
     key: 'WBNB',
     value: 'Wrraped BNB',
+    defaultPaymentMethod:true,
   },
   {
     chainId: 43114,
     address: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
     key: 'WAVAX',
     value: 'Wrraped AVAX',
+    defaultPaymentMethod:true,
   },
   {
     chainId: 137,
     address: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
     key: 'WMATIC',
     value: 'Wrraped MATIC',
+    defaultPaymentMethod:true,
   },
   {
     chainId: 1285,
     address: '0x98878B06940aE243284CA214f92Bb71a2b032B8A',
     key: 'WMOVR',
     value: 'Wrraped MOVR',
+    defaultPaymentMethod:true,
   },
   {
     chainId: 1284,
     address: '0xAcc15dC74880C9944775448304B263D191c6077F',
     key: 'WGLMR',
     value: 'Wrraped GLMR',
+    defaultPaymentMethod:true,
   },
   
   // TESTNET
@@ -42,11 +48,13 @@ export default [
     address: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
     key: 'WRIN',
     value: 'Wrraped RINKEBY',
+    defaultPaymentMethod:true,
   },
   {
     chainId: 43113,
     address: '0xd00ae08403b9bbb9124bb305c09058e32c39a48c',
     key: 'WFUJI',
     value: 'Wrraped FUJI',
+    defaultPaymentMethod:true,
   },
 ]
