9d632ab6e147b8d653ec4ca103270c8dd867561e
alonNftrade <94784334+alonNftrade@users.noreply.github.com>
2022-07-27 10:01:04 +0300

    return config

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ import { PROD, TESTNET } from "./constants"
 export default {
     api: {
         [PROD]: 'https://api.nftrade.com/api/v1/',
-        [TESTNET]: 'http://localhost:4040/api/v1/',
+        [TESTNET]: 'https://api-testnets.nftrade.com/api/v1/',
     }
 }
