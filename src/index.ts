a013af21ad476392b740adf623ffcf4250272290
alonNftrade <94784334+alonNftrade@users.noreply.github.com>
2022-08-14 17:34:30 +0300

    export transaction by default

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * as types from "./types";
 export * as addresses from "./addresses";
 export { default as Contracts } from "./transaction/contracts";
 export { Wallet } from "./wallet/Wallet";
-
+export { default as TransactionEvm } from "./transaction/TransactionEvm";
+export { default as TransactionImx } from "./transaction/TransactionImx";
 export default Nifty
