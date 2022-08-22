1a082523d78140aa2227686f629bad9b33cb583b
alonNftrade <94784334+alonNftrade@users.noreply.github.com>
2022-08-22 14:17:17 +0300

    update http requst name to folow naming convention

--- src/libs/api/users.js
+++ src/libs/api/users.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var HttpRequest_1 = require("../utils/HttpRequest");
+var httpRequest_1 = require("../utils/httpRequest");
 var config_1 = require("../../config");
 var URL = "".concat(config_1.default.api, "users");
 var get = function (token, id, params) {
     if (params === void 0) { params = {}; }
-    return (0, HttpRequest_1.default)({
+    return (0, httpRequest_1.default)({
         url: "".concat(URL, "/").concat(id),
         headers: {
             'x-access-token': token,
@@ -17,12 +17,12 @@ var get = function (token, id, params) {
 };
 var getAll = function (params) {
     if (params === void 0) { params = {}; }
-    return (0, HttpRequest_1.default)({ url: "".concat(URL), params: params });
+    return (0, httpRequest_1.default)({ url: "".concat(URL), params: params });
 };
-var getByAddress = function (userAddress) { return (0, HttpRequest_1.default)({ url: "".concat(URL, "/address/").concat(userAddress) }); };
+var getByAddress = function (userAddress) { return (0, httpRequest_1.default)({ url: "".concat(URL, "/address/").concat(userAddress) }); };
 var count = function (params) {
     if (params === void 0) { params = {}; }
-    return (0, HttpRequest_1.default)({ url: "".concat(URL, "/get/count"), params: params });
+    return (0, httpRequest_1.default)({ url: "".concat(URL, "/get/count"), params: params });
 };
 exports.default = {
     get: get,
