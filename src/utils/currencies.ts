a5744b962bbae2292a0c26427b031e8ba803b1ef
zetyliam <liam@nftrade.com>
2022-09-01 10:09:46 +0300

    added goerli currency

--- src/utils/currencies.ts
+++ src/utils/currencies.ts
@@ -1,4 +1,56 @@
 export default [
+
+  {
+    chainId: 1,
+    address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    key: 'WETH',
+    value: 'Wrapped Ether',
+    defaultPaymentMethod:true,
+  },
+  {
+    chainId: 56,
+    address: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+    key: 'WBNB',
+    value: 'Wrapped BNB',
+    defaultPaymentMethod:true,
+  },
+  {
+    chainId: 43114,
+    address: '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7',
+    key: 'WAVAX',
+    value: 'Wrapped AVAX',
+    defaultPaymentMethod:true,
+  },
+  {
+    chainId: 137,
+    address: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+    key: 'WMATIC',
+    value: 'Wrapped MATIC',
+    defaultPaymentMethod:true,
+  },
+  {
+    chainId: 1285,
+    address: '0x98878b06940ae243284ca214f92bb71a2b032b8a',
+    key: 'WMOVR',
+    value: 'Wrapped MOVR',
+    defaultPaymentMethod:true,
+  },
+  {
+    chainId: 1284,
+    address: '0xacc15dc74880c9944775448304b263d191c6077f',
+    key: 'WGLMR',
+    value: 'Wrapped GLMR',
+    defaultPaymentMethod:true,
+  },
+  
+  // TESTNET
+  {
+    chainId: 4,
+    address: '0xc778417e063141139fce010982780140aa0cd5ab',
+    key: 'WRIN',
+    value: 'Wrapped RINKEBY',
+    defaultPaymentMethod:false,
+  },
   {
     chainId: 4,
     address: '0x0000000000000000000000000000000000000000',
@@ -7,61 +59,24 @@ export default [
     defaultPaymentMethod:true,
   },
   {
-    chainId: 1,
-    address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-    key: 'WETH',
-    value: 'Wrraped Ether',
-    defaultPaymentMethod:true,
-  },
-  {
-    chainId: 56,
-    address: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-    key: 'WBNB',
-    value: 'Wrraped BNB',
-    defaultPaymentMethod:true,
-  },
-  {
-    chainId: 43114,
-    address: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
-    key: 'WAVAX',
-    value: 'Wrraped AVAX',
-    defaultPaymentMethod:true,
-  },
-  {
-    chainId: 137,
-    address: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
-    key: 'WMATIC',
-    value: 'Wrraped MATIC',
-    defaultPaymentMethod:true,
-  },
-  {
-    chainId: 1285,
-    address: '0x98878B06940aE243284CA214f92Bb71a2b032B8A',
-    key: 'WMOVR',
-    value: 'Wrraped MOVR',
-    defaultPaymentMethod:true,
-  },
-  {
-    chainId: 1284,
-    address: '0xAcc15dC74880C9944775448304B263D191c6077F',
-    key: 'WGLMR',
-    value: 'Wrraped GLMR',
-    defaultPaymentMethod:true,
-  },
-  
-  // TESTNET
-  {
-    chainId: 4,
-    address: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
-    key: 'WRIN',
-    value: 'Wrraped RINKEBY',
+    chainId: 5,
+    address: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
+    key: 'WGOETH',
+    value: 'Wrapped GOETH',
     defaultPaymentMethod:false,
+  },
+  {
+    chainId: 5,
+    address: '0x0000000000000000000000000000000000000000',
+    key: 'ETH',
+    value: 'Ether',
+    defaultPaymentMethod:true,
   },
   {
     chainId: 43113,
     address: '0xd00ae08403b9bbb9124bb305c09058e32c39a48c',
     key: 'WFUJI',
-    value: 'Wrraped FUJI',
+    value: 'Wrapped FUJI',
     defaultPaymentMethod:true,
   },
 ]
